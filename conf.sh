ed91c38ebe53614ba624208e297778334099d23b
paramdl <113905584+paramdl@users.noreply.github.com>
2023-02-17 11:29:45 +0530

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -5,4 +5,7 @@ sudo sed -i '59 i\<role rolename="manager-status"/>' /home/ec2-user/apache-tomca
 sudo sed -i '60 i\<user username="admin" password="admin" roles="manager-gui, manager-script, manager-jmx,manager-status"/>' /home/ec2-user/apache-tomcat-9.0.71/conf/tomcat-users.xml
 sudo sed -i '19,1 i\<!--' /home/ec2-user/apache-tomcat-9.0.71/webapps/manager/META-INF/context.xml
 sudo sed -i '24,1 i\-->' /home/ec2-user/apache-tomcat-9.0.71/webapps/manager/META-INF/context.xml
-sudo ./home/ec2-user/apache-tomcat-9.0.71/startup.sh
+sudo su -
+cd /home/ec2-user
+./apache-tomcat-9.0.71/bin/startup.sh
+
